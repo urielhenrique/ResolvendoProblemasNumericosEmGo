7b88f88a8640287d52d5495bad217b7b104d4713
Uriel Henrique <64621509+urielhenrique@users.noreply.github.com>
2023-08-22 07:18:53 -0300

    Add files via upload

---
+++ desafio2.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main (){
+
+for i := 1; i <= 100; i++ {
+
+	 if i%3 == 0 {
+		fmt.Println("Pan")
+	 }
+	 if i%5 == 0{
+		fmt.Println("Pin")
+	 } 
+	 if i%3 ==0 && i%5 ==0 {
+		fmt.Println("Pin e Pan")
+	 }	
+	}
+}
